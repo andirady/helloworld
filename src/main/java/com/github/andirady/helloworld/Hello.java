357bda229c1538993e27981104ac39ea8b7bcd23
andirady <andirady@kendra-solutions.com>
2022-04-29 22:01:01 +0800

    Initial commit

---
+++ src/main/java/com/github/andirady/helloworld/Hello.java
@@ -0,0 +1,11 @@
+package com.github.andirady.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        new Hello().sayHello();
+    }
+
+    public void sayHello() {
+        System.out.println("Hello world");
+    }
+}
